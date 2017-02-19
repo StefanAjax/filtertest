6387602f73fbc36c0bdd6b824ce4276e9b59e24f
StefanAjax <pianoajax@gmail.com>
2017-02-19 13:02:29 +0100

    ll

--- filtercode.py
+++ filtercode.py
@@ -2,16 +2,11 @@ import numpy as np
 Fs = 44100
 T = 1/Fs
 
-print("Fs =",Fs,"Hz")
-print("T =",T,"s")
+print("Fs =", Fs, "Hz")
+print("T =", T, "s")
 
 prin(32)
 
-
-
-t = np.arange(0,1,1/Fs)
+t = np.arange(0, 1, 1/Fs)
 
 print(t)
-
-
-
